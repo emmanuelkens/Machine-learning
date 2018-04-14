d8b3364431d6b8e80f725c573422b729c4832374
Emmanuelkens <emmanuelkens@gmail.com>
2018-04-14 22:51:01 +0300

    Create svm.py

---
+++ svm.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm
+from matplotlib import style
+style.use("ggplot")
+#x = [1,5,1.5,8,1,9]
+#y = [2,8,1.8,8,0.6,11]
+#plt.scatter(x,y)
+
+X = np.array([[1,2],
+             [5,8],
+             [1.5,1.8],
+             [8,8],
+             [1,0.6],
+             [9,11]])
+y = np.array([0,1,0,1,0,1])
+z = np.array([[0.56,0.76]])
+clf = svm.SVC(kernel='linear', C = 1.0).fit(X,y)
+print(clf.predict(z))
+w = clf.coef_[0]
+print(w)
+a = -w[0] / w[1]
+
+xx = np.linspace(0,12)
+yy = a * xx - clf.intercept_[0] / w[1]
+
+h0 = plt.plot(xx, yy, 'k-', label="non weighted div")
+
+plt.scatter(X[:, 0], X[:, 1], c = y)
+plt.legend()
+plt.show()
